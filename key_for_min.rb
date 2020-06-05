24dc292aeeb4c6486e02973d1c0a606d49c997bb
Daniel Andrew <danwandrew@gmail.com>
2020-06-05 14:53:18 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -14,3 +14,14 @@ def key_for_min_value(name_hash)
   name_hash.each{|k,v| low_name = [k, v] if low_name[1] > v }
   low_name[0]
   end
+  
+#   def key_for_min_value(name_hash)
+#   empty_hash = nil
+#   name_hash.collect do |name,value|
+#     if value <= 1
+#       return name
+#     end
+#   end
+#   empty_hash
+# end
+
